bd3d7b341724595bb9b24de4d3701a6cc74ea1b4
Alex Menendez <alex.menendez@docket.com.br>
2019-01-18 15:54:51 -0200

    token set via requisicao

--- src/main/java/br/com/docket/uploaderToMotherbird/controller/SolicitacaoController.java
+++ src/main/java/br/com/docket/uploaderToMotherbird/controller/SolicitacaoController.java
@@ -1,11 +1,13 @@
 package br.com.docket.uploaderToMotherbird.controller;
 
+import br.com.docket.uploaderToMotherbird.motherbird.Token;
 import br.com.docket.uploaderToMotherbird.motherbird.service.MotherbirdService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -17,6 +19,9 @@ import java.util.Set;
 @Controller
 @RequestMapping("/solicitacoes")
 public class SolicitacaoController {
+	
+	@Autowired
+	private Token token;
 
 	@Autowired
 	private MotherbirdService motherbirdService;
@@ -29,14 +34,16 @@ public class SolicitacaoController {
 	}
 
 	@PostMapping
-	public String novaSolicitacao(Model model, List<MultipartFile> multipartFileList) throws IOException {
+	public String novaSolicitacao(Model model, List<MultipartFile> multipartFileList, @RequestHeader(value = "Authorization", required = true) String token) throws IOException {
 
+		this.token.setToken(token);
+		
 		if(multipartFileList.isEmpty()) {
 			return "redirect:/solicitacoes";
 		}
 
 		for(MultipartFile multipartFile: multipartFileList) {
-			motherbirdService.sendToMotherbird(multipartFile.getBytes(), multipartFile.getContentType());
+			motherbirdService.sendToMotherbird(multipartFile.getBytes(), "application/pdf");
 		}
 
 		model.addAttribute("success", true);
--- src/main/java/br/com/docket/uploaderToMotherbird/motherbird/Token.java
+++ src/main/java/br/com/docket/uploaderToMotherbird/motherbird/Token.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Token {
-	private String token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJtb3RoZXJiaXJkIiwicm9sZSI6IlJPTEVfQURNSU4iLCJjcmVhdGVkIjoxNTI2MDQyNDExMzU0LCJleHAiOjE1Mjg2MzQ0MTF9.PKDwJoNuWOGXMH7TkdTL6To6E25iIL-btrmHGkhnFYjIhjVjXhMpqDRl1dewvLFtfak3YMGSGnU376Xz3cI4wQ";
+	private String token = "";
 
 	public String getToken() {
 		return token;
